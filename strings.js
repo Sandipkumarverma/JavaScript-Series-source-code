a8f529c2820f35811242864c7b4e01623f695c85
sandip kr verma <145849942+Sandipkumarverma@users.noreply.github.com>
2024-12-23 06:36:34 +0530

    Add files via upload
    Strings in JavaScript And  Strings Methods | JS Tutorial for Beginners | JavaScript full course in hindi for beginners.

---
+++ strings.js
@@ -0,0 +1,63 @@
+let Name = "Sandip"
+const course = 'Javascript'
+//console.log(`learn ${course} through ${Name}`);
+
+//console.log("sandip",Name.length);
+//console.log("javascript",course.length);
+
+//let channel = new String("NextEra Coders")
+//console.log(channel);
+
+
+let NaLc = Name.toLowerCase()
+//console.log(NaLc);
+//console.log(Name.toLowerCase());
+
+let couUc = course.toUpperCase()
+//console.log(couUc);
+//console.log(course.toUpperCase());
+//console.log(course);
+
+let NameCourse = Name.concat(course)
+//console.log(NameCourse);
+//console.log(Name.concat(course));
+
+//let Nasl = Name.slice(0,7)
+//console.log(Nasl);
+//console.log("sandip",Name.slice(0,4));
+
+//let Nasl = course.substring(0,8)
+//console.log(Nasl);
+//console.log("sandip",Name.substring(0,4));
+
+let courseChar = course.charAt(8)
+//console.log(courseChar);
+//console.log(course.charAt(4));
+
+let courseindex = course.indexOf(("v")) 
+//console.log(courseindex);
+//console.log("javascript",course.indexOf("t"));
+ 
+let string = "Js learn in NextEra Coders";
+let Newstr = string.replace("Js", "javascript")
+//console.log(Newstr);
+//console.log(string);
+//console.log(string.replace("Js", "javascript"));
+
+
+let strinc = string.includes("Next")
+//console.log(strinc);
+//console.log(string.includes("HTML"));
+
+
+//console.log(string.split());
+//console.log(string.split(""));
+//console.log(string.split(" "));
+
+
+let owner = "          sandip          "
+console.log(owner.trim());
+console.log(owner.trimStart());
+console.log(owner.trimEnd());
+
+
