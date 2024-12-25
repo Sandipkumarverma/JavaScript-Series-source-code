c2175960a7594ab11e8cdebde9fdf7d87d1c5d39
sandip kr verma <145849942+Sandipkumarverma@users.noreply.github.com>
2024-12-25 16:36:21 +0530

    Add files via upload

---
+++ scope.js
@@ -0,0 +1,45 @@
+
+
+// let var1 = "NextEra"
+// const var2 = "Coders"
+// var var3 = "NextEra Coders"
+
+
+// var c = 300
+// console.log(var1);
+// console.log(var2);
+// console.log(var3);
+
+
+{
+    // let a = 10
+    // const b = 20
+    // var c = 30
+    
+}
+
+//console.log(a);
+//console.log(b);
+//console.log(c);
+
+
+// let var1 = "NextEra"
+// const var2 = "Coders"
+// var var3 = "NextEra Coders"
+
+
+// function Myfunction(){
+//     let var1 = "NextEra"
+//     const var2 = "Coders"
+//     var var3 = "NextEra Coders" 
+
+    
+    
+// }
+// Myfunction()
+//console.log(var1);
+  //  console.log(var2);
+    //console.log(var3);
+
+
+    
