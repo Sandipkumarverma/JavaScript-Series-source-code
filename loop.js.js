82273822bc68fc8f1bfb323a1521766888d90be1
sandip kr verma <145849942+Sandipkumarverma@users.noreply.github.com>
2024-12-25 15:01:03 +0530

    Add files via upload

---
+++ loop.js.js
@@ -0,0 +1,163 @@
+// console.log("NextEra Coders");
+// console.log("NextEra Coders");
+// console.log("NextEra Coders");
+// console.log("NextEra Coders");
+// console.log("NextEra Coders");
+
+// for(let i=0; i<5000; i++){
+//     console.log("NextEra Coders");
+    
+// }
+
+// for(let i=1; i<=15; i++){
+//     console.log(i);
+    
+// }
+
+
+// let sum=0;
+// for(let i=0; i<=150; i++){
+//     sum= sum+i;
+
+// }
+// console.log("sum = "+sum);
+
+
+// for(let i=0; i<=100; i++){
+//     console.log(i);
+    
+// }
+
+// for(let i=1; i<=100; i++){
+//     if(i%2!=0){
+//         console.log(i+" is odd Number");
+        
+//     }
+// }
+
+// for(let i=1; i<=10; i++){
+//     for(let j=1; j<=10; j++){
+//         console.log(i+" * "+j+" = "+i*j);
+        
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     while loop 
+
+// let i=1;
+// while(i<=15){
+//     console.log(i);
+//     i++;
+    
+// }
+
+
+// let arr = [10, 20, 30, 40, 50]
+// let count = 0;
+// while(count<arr.length){
+//     console.log(arr[count]);
+//     count++;
+    
+// }
+
+
+
+//                  Do while loop
+
+
+// let count=0;
+// do{
+//     console.log(count);
+//     count++;
+    
+// }while(count<=10)
+
+
+//                          For in loop 
+
+
+// const arr = [10, 20, 30, 40, 50]
+// for(let i in arr){
+//     console.log("index = "+i+" value = "+arr[i]);
+    
+// }
+
+
+// let js = {
+//     channel : "NextEra Coders",
+//     course : "javascript",
+//     chapter : "loop",
+//     year : "2024"
+// }
+// for(let key in js){
+//     console.log(`${key} ${js[key]}`);
+    
+// }
+
+
+
+
+//                            for of loop
+
+
+
+// let arr = [10, 20, 30, 40]
+// for(let i of arr){
+//     console.log(i);
+    
+// }
+
+
+
+// let str = "javascript";
+// for(let i of str){
+//     console.log(i);
+    
+// }
+
+
+// let m =new Map([
+//     ["channel", "NextEra Coders"],
+//     ["course","javascript"],
+//     ["chapter","loop"],
+//     ["year","2024"]
+// ]);
+// for(let [key, value] of m){
+//     console.log(`${key} : ${value}`);
+    
+// }
+
+// let js = new Set([1,2,3,4,5])
+// for(let value of js){
+//     console.log(value);
+    
+// }
