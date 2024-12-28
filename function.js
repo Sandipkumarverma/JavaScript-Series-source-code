cf81dcf80f33ef813d527dabcc77c53cd926b44b
sandip kr verma <145849942+Sandipkumarverma@users.noreply.github.com>
2024-12-28 16:11:33 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,152 @@
+// console.log("Hello Coders!");
+// console.log("Welcome to NextEra Coders");
+
+
+
+// function MyFunction(){
+//     console.log("Hello Coders!");
+//     console.log("Welcome to NextEra Coders");
+//     console.log("Hello Coders!");
+//     console.log("Welcome to NextEra Coders");
+// }
+// MyFunction()
+// MyFunction()
+// MyFunction()
+
+
+// function MyFunction(){
+//     console.log("NextEra Coders");
+    
+// }
+// MyFunction()
+// MyFunction()
+// MyFunction()
+// MyFunction()
+
+
+// function MyFunction(name){          // parameter
+//     console.log(`Hello ${name} Welcome to NextEra Coders`);
+    
+// }
+// MyFunction("Shivam")                // arguments
+
+
+
+
+// function add(a,b){
+
+//     console.log("Before return");
+    
+//     return a+b;
+//     console.log("after return");
+    
+    
+// }
+// console.log(add(5,8));
+
+
+
+
+// function add(a,b){     
+//     let sum = a+b;
+//     return   sum;
+    
+// }
+// console.log(add(5,8));
+
+
+
+// function add(a,b){     
+//     let sum = a+b;
+//     return   sum;
+    
+// }
+// let addTwonum = add(5,8)
+// console.log(addTwonum);
+
+
+
+
+// function MyFunction(){
+//     return "we are learn js in NextEra Coders"
+// }
+// console.log(MyFunction());
+
+
+
+// function MyFunction(){
+//     let course = "we are learn js in NextEra Coders"
+//     return course
+// }
+// console.log(MyFunction());
+
+
+
+//              Function Expression
+
+// let MyFunction = function(a,b){
+//     return a+b;
+// }
+
+// console.log(MyFunction(5,9));
+
+
+
+// let MyFunction = function(a,b){
+//     let sum = a+b;
+//     return sum
+// }
+
+// console.log(MyFunction(5,9));
+
+// let MyFunction = function(a,b){
+//     let sum = a+b;
+//     return sum
+// }
+//  let addTwoNum = MyFunction(5,9)
+// console.log(addTwoNum);
+
+
+
+
+
+// let MyFunction = function(){
+//     console.log("We are learn js");
+    
+// }
+// MyFunction()
+
+
+
+
+
+// let mul = (a,b) => a*b
+// console.log(mul(9,8));
+
+
+
+
+// let greater = (a,b) =>{
+//     if(a>b){
+//         console.log(`${a} a is greater than b ${b}`);
+        
+//     }
+//     else{
+//         console.log(`${b}  b is greater than a ${a}`);
+        
+//     }
+// }
+// greater(45,23)
+
+
+
+let oddEven  = (a) =>{
+    if(a%2==0){
+        console.log(`${a} is even number`);
+        
+    }else{
+        console.log(`${a} is odd number`);
+        
+    }
+}
+oddEven(9)
