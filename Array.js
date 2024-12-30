27f68bb4e060cf55fcc6a6da0f2f2b4b7d84294c
sandip kr verma <145849942+Sandipkumarverma@users.noreply.github.com>
2024-12-30 16:45:06 +0530

    Add files via upload

---
+++ Array.js
@@ -0,0 +1,134 @@
+// let arr = []
+// console.log(arr);
+
+// let course = ["Html", "css", "javascript"]
+// let arr = [10, 20, 30, 40, 50]
+
+
+//console.log(arr);
+//console.log(arr[1]);
+
+//console.log(typeof arr);
+
+// let first = arr[0]                    //  arr first element
+// console.log(first);
+
+//console.log(course[course.length-1]);          // arr last element
+
+//console.log(course);
+
+//course[3] = "React JS"
+//course[1] = "Bootstrap"            // modifying in arr
+//console.log(course);
+
+//course.push("Vue JS")
+//course.push(500)                     // Insert element using push Methos
+//console.log(course);
+
+//console.log(course);
+
+//course.unshift("React js")     //        unshift method
+// console.log(course);
+
+
+// course.pop()
+// console.log(course);              //       pop Method
+// course.pop()
+ //console.log(course);
+
+
+//course.shift()                     //   shift method
+//console.log(course);
+
+
+
+
+//console.log(arr);
+
+//console.log(arr.length);
+
+// console.log(course);
+// console.log(course.length);             arr lenght
+
+// course.length = 8
+// console.log(course);
+
+
+// course.length = 2
+// console.log(course);
+
+
+
+//                            print arr value using for loop
+// console.log(arr);
+
+// for(i=0; i<arr.length; i++){
+//     console.log(arr[i]);
+    
+// }
+
+
+// console.log(course);
+
+// for(i=0; i<course.length; i++){
+//     console.log(course[i]);
+    
+// }
+
+
+
+//                             foreach loop
+
+// arr.forEach(function Myfunction(value){
+//     console.log(value);
+    
+// })
+
+
+
+// course.forEach(function Myfunction(value){
+//     console.log(value);
+    
+// })
+
+
+
+let course = ["Html", "css", "javascript"]
+let arr = [100, 20, 3, 40, 500]
+let Webdev = ["React Js", "Node Js", "Mongo db"]
+
+
+//console.log(arr.concat(course));
+
+// let CourseWebdev = course.concat(Webdev)
+// console.log(CourseWebdev);
+
+//console.log(course);
+
+// let arrstr = course.toString()                   arr to string
+// console.log(arrstr);          
+
+
+
+//console.log(course.join("|"));                   join method
+
+
+ console.log(arr);
+
+// console.log(arr.reverse());            // Reverse method
+
+
+//console.log(arr.includes(20));
+
+
+// function arrsort(a,b){                //  sort method 
+//     return a-b;
+// }
+// let asort = arr.sort(arrsort)
+// console.log(asort);
+
+
+//console.log(arr.slice(2,6));            // slice method
+
+// console.log(arr.splice(0,6));        // splice method
+ 
