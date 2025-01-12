6366d7a49cc8999fc0ef6a2fe2c274fdab5ec290
sandip kr verma <145849942+Sandipkumarverma@users.noreply.github.com>
2025-01-12 13:13:11 +0530

    Add files via upload

---
+++ this.js
@@ -0,0 +1,109 @@
+//console.log(this);
+
+
+// let name = this;
+// console.log(name);
+
+
+//console.log(typeof this);
+
+
+// function person () {
+//      console.log(this); 
+// }
+// person()
+
+
+
+// let naem = () => {
+//      console.log(this);   
+// }
+// naem()
+
+
+
+
+
+
+// let courseDetails = {
+//     course : "JavaScript",
+//     chapter : "this keyword",
+    
+//     sayHello (){
+//         console.log(`we are learning ${courseDetails.course} in ${ChlDetails.Channel}`);
+        
+//     }
+// }
+
+// let ChlDetails = {
+//     Channel : "NextEra Coders",
+//     name : "sandip"
+    
+// }
+//  courseDetails.sayHello();
+
+
+
+
+// const person = {
+//      firstName : "sandip",
+//      lastName : "Verma",
+//      fullName : function () {
+//           return this.firstName + this.lastName;
+//      }
+
+// }
+// console.log(person.fullName());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function add(a,b){
+//   return a*b;
+// }
+// let Result = add.call(this,9,4)
+// console.log(Result);
+
+
+// function student(name,age,branch){
+//   this.name = name;
+//   this.age = age;
+//   this.branch = branch;
+// }
+
+// let Student = new student("sandip",25,"cse")
+
+// console.log(Student.name);
+// console.log(Student.age);
+// console.log(Student.branch);
+
+// console.log(Student);
